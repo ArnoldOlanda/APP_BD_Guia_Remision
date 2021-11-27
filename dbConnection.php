9904d0306fdfd55bf7603a5101edf00cd913c124
ArnoldOlanda <olanda188@gmail.com>
2021-11-27 00:13:24 -0500

    V3.0.1 agregado archivo README y cambiada la contraseña de conexion a la base de datos

--- dbConnection.php
+++ dbConnection.php
@@ -7,7 +7,7 @@
              if(!isset(self::$instancia))
              {
                  $opcionesPDO[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-                 self::$instancia= new PDO('mysql:host=localhost;dbname=bd_guia_remision','root','admin',$opcionesPDO);
+                 self::$instancia= new PDO('mysql:host=localhost;dbname=bd_guia_remision','root','',$opcionesPDO);
                  //echo "conexion realizada";
              }
              return self::$instancia;
